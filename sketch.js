92035b0582ca6ecd6a5d475a04b0f9fd110fb092
yugsalecha41 <86595254+yugsalecha41@users.noreply.github.com>
2021-08-11 10:40:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1010,800);
+
+	var ball_option={
+		isStatic:false,
+		restitution:0.3,
+		friction:0,
+		dencity:1.2
+	}
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
